3931b158d73f42727efece4cdf7b9a56682f0cff
Anna <a1b2v3@mail.ru>
2022-12-07 16:18:05 +0500

    add Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+ int x = 0;
+ int y = 0;
+ int x2 = 1;
+ int y2 = 1;
+ double s = Math.Pow((Math.Pow((x+x2),2) + Math.Pow((y+y2),2)),0.5);
+
+Console.WriteLine(s);
