1b9a04acffb900590d3aeae2866c325c626a226a
anna <a1b2v3@mail.ru>
2022-12-08 00:14:00 +0500

    add Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int n = Convert.ToInt32(Console.ReadLine());
+int a = n/10000;
+int b = n%10;
+int c = (n/1000)%10;
+int d = (n%100)/10;
+if  (a == b  &  c == d)
+{
+ Console.WriteLine("Палиндром");   
+}
+else
+{
+Console.WriteLine("Нет");
+} 
+
